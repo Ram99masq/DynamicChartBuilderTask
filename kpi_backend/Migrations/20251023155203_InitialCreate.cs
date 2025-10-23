b594062c85d07a34eeb370fbb93487ad2d67abe9
ram99.masq <ram99.masq@gmail.com>
2025-10-23 22:06:44 +0530

    Added code migration to database to reflect changes

---
+++ kpi_backend/Migrations/20251023155203_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kpi_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Detections",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Class = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    X = table.Column<float>(type: "real", nullable: false),
+                    Y = table.Column<float>(type: "real", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Speed = table.Column<float>(type: "real", nullable: false),
+                    Heading = table.Column<float>(type: "real", nullable: false),
+                    Vest = table.Column<bool>(type: "bit", nullable: false),
+                    Zone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Detections", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KPIPresets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Metric = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Filters = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GroupBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChartType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KPIPresets", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Detections");
+
+            migrationBuilder.DropTable(
+                name: "KPIPresets");
+        }
+    }
+}
