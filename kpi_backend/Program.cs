64d18a3900067f37ef141f570b99d4c0c9f2dadf
ram99.masq <ram99.masq@gmail.com>
2025-10-23 20:08:04 +0530

    Added the sql server connection object using entity framework code.

--- kpi_backend/Program.cs
+++ kpi_backend/Program.cs
@@ -1,3 +1,7 @@
+using kpi_backend.Data;
+using kpi_backend.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Add the Database connection contect to the sql server DB
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IDataService, DataService>();
 
 var app = builder.Build();
 
